68826d6ea62f2d3a844f540125dc437de52c70a1
Hristo Karchev <x_karchev@mail.bg>
2021-09-30 22:21:34 +0300

    NavLinks, NavItem, NavMenu and MobileIcon created

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import { Nav, NavbarContainer, NavLogo } from './NavbarElements';
+import { FaBars } from 'react-icons/fa';
+
+import {
+    Nav,
+    NavbarContainer,
+    NavLogo,
+    MobileIcon,
+    NavMenu,
+    NavItem,
+    NavLinks
+} from './NavbarElements';
 
 const Navbar = () => {
     return (
         <>
             <Nav>
                 <NavbarContainer>
-                    <NavLogo>
-                        Alpha
-                    </NavLogo>
+                    <NavLogo to="/">Alpha</NavLogo>
+                    <MobileIcon>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to="about">About</NavLinks>
+                        </NavItem>
+                    </NavMenu>
                 </NavbarContainer>
             </Nav>
         </>
