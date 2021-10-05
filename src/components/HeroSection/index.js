611489a08f3873161fb0f16d1d776c7c851223e5
Hristo Karchev <x_karchev@mail.bg>
2021-10-05 21:04:22 +0300

    more styles for the home page

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import Video from '../../videos/video3.mp4';
-import { HeroContainer, HeroBG, VideoBG } from './HeroElements';
+import React, { useState } from 'react';
+import Video from '../../videos/video2.mp4';
+import {
+    HeroContainer,
+    HeroBG,
+    VideoBG,
+    HeroContent,
+    HeroH1,
+    HeroP,
+    HeroBtnWrapper,
+    ArrowForward,
+    ArrowRight
+} from './HeroElements';
 
 function HeroSection() {
+    const [hover, setHover] = useState(false);
+
+    const onHover = () => {
+        setHover(!hover)
+    }
+
+
     return (
         <HeroContainer>
             <HeroBG>
                 <VideoBG autoPlay loop muted src={Video} type='video/mp4' />
             </HeroBG>
-          {/*   <HeroContent>
+            <HeroContent>
                 <HeroH1>Text</HeroH1>
                 <HeroP>
                     Sign up for a new acc today and receive ...
                 </HeroP>
                 <HeroBtnWrapper>
-                    <Button to="signup">
+                    <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}>
                         Get Started {hover ? <ArrowForward /> : <ArrowRight />}
                     </Button>
                 </HeroBtnWrapper>
-            </HeroContent> */}
+            </HeroContent>
         </HeroContainer>
     )
 }
