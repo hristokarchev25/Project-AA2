75b95d25036c50260759ad5ecf68ab15b61f0334
Hristo Karchev <x_karchev@mail.bg>
2021-10-05 21:47:45 +0300

    Scroll Button style

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Video from '../../videos/video2.mp4';
+import { Button } from '../ButtonElement';
 import {
     HeroContainer,
     HeroBG,
@@ -26,12 +27,16 @@ function HeroSection() {
                 <VideoBG autoPlay loop muted src={Video} type='video/mp4' />
             </HeroBG>
             <HeroContent>
-                <HeroH1>Text</HeroH1>
+                <HeroH1>Tell The Vision</HeroH1>
                 <HeroP>
-                    Sign up for a new acc today and receive ...
+                    Sign up for a new account today!
                 </HeroP>
                 <HeroBtnWrapper>
-                    <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}>
+                    <Button to="signup" onMouseEnter={onHover}
+                        onMouseLeave={onHover}
+                        primary="true"
+                        dark="true"
+                    >
                         Get Started {hover ? <ArrowForward /> : <ArrowRight />}
                     </Button>
                 </HeroBtnWrapper>
