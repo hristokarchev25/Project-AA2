adda551afd4bf5b4b94ab4e482155c3c7554767d
Hristo Karchev <x_karchev@mail.bg>
2021-10-29 23:05:15 +0300

    Data file created
    File with objs + styles

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,18 @@
+import svgOne from '../../images/welcome.svg';
+
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headline: 'Unlimited Trans with zero fees',
+    description: 'Get access ..... text.',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    img: svgOne,
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+};
--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Button } from '../ButtonElement';
+
 import {
     Column2,
     Img,
@@ -12,28 +14,54 @@ import {
     Heading,
     Subtitle,
     BtnWrap,
-    
 } from './InfoElements';
 
-function InfoSection() {
+function InfoSection({
+    lightBg,
+    id,
+    imgStart,
+    topLine,
+    lightText,
+    headline,
+    darkText,
+    description,
+    buttonLabel,
+    img,
+    alt,
+    primary,
+    dark,
+    dark2
+}) {
     return (
         <>
-            <InfoContainer>
+            <InfoContainer lightBg={lightBg} id={id}>
                 <InfoWrapper>
-                    <InfoRow>
+                    <InfoRow imgStart={imgStart}>
                         <Column1>
                             <TextWrapper>
-                                <TopLine>TopLine</TopLine>
-                                <Heading>Heading</Heading>
-                                <Subtitle>Subtitle</Subtitle>
+                                <TopLine>{topLine}</TopLine>
+                                <Heading lightText={lightText}>{headline}</Heading>
+                                <Subtitle darkText={darkText}>{description}</Subtitle>
                                 <BtnWrap>
-                                    <Button to="home">Btn</Button>
+                                    <Button
+                                        to="home"
+                                        smooth={true}
+                                        duration={500}
+                                        spy={true}
+                                        exact="true"
+                                        offset={-80}
+                                        primary={primary ? 1 : 0}
+                                        dark={dark ? 1 : 0}
+                                        dark2={dark2 ? 1 : 0}
+                                    >
+                                        {buttonLabel}
+                                    </Button>
                                 </BtnWrap>
                             </TextWrapper>
                         </Column1>
                         <Column2>
                             <ImgWrap>
-                                <Img />
+                                <Img src={img} alt={alt} />
                             </ImgWrap>
                         </Column2>
                     </InfoRow>
