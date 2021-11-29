fe6e457f70a9ed441b52c1c86637762740d7844a
Hristo Karchev <x_karchev@mail.bg>
2021-11-29 17:09:47 +0200

    Footer skeleton created/ without style

---
+++ src/components/Footer/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <FooterContainer>
+                <FooterWrap>
+                    <FooterLinksContainer>
+                        <FooterLinksWrapper>
+
+                            <FooterLinkItems>
+                                <FooterLinkTitle>About us</FooterLinkTitle>
+                                <FooterLink to="/signin">How it works</FooterLink>
+                                <FooterLink to="/signin">Test</FooterLink>
+                                <FooterLink to="/signin">Yoga</FooterLink>
+                                <FooterLink to="/signin">Terms of Service</FooterLink>
+                            </FooterLinkItems>
+
+                            <FooterLinkItems>
+                                <FooterLinkTitle>About us</FooterLinkTitle>
+                                <FooterLink to="/signin">How it works</FooterLink>
+                                <FooterLink to="/signin">Test</FooterLink>
+                                <FooterLink to="/signin">Yoga</FooterLink>
+                                <FooterLink to="/signin">Terms of Service</FooterLink>
+                            </FooterLinkItems>
+
+                        </FooterLinksWrapper>
+
+                        <FooterLinksWrapper>
+
+                            <FooterLinkItems>
+                                <FooterLinkTitle>About us</FooterLinkTitle>
+                                <FooterLink to="/signin">How it works</FooterLink>
+                                <FooterLink to="/signin">Test</FooterLink>
+                                <FooterLink to="/signin">Yoga</FooterLink>
+                                <FooterLink to="/signin">Terms of Service</FooterLink>
+                            </FooterLinkItems>
+
+                            <FooterLinkItems>
+                                <FooterLinkTitle>About us</FooterLinkTitle>
+                                <FooterLink to="/signin">How it works</FooterLink>
+                                <FooterLink to="/signin">Test</FooterLink>
+                                <FooterLink to="/signin">Yoga</FooterLink>
+                                <FooterLink to="/signin">Terms of Service</FooterLink>
+                            </FooterLinkItems>
+
+                        </FooterLinksWrapper>
+                    </FooterLinksContainer>
+                </FooterWrap>
+            </FooterContainer>
+        </>
+    )
+}
+
+export default Footer;
