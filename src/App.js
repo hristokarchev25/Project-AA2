3d061653e4529c317d3f0344bbbf555efc21174f
Hristo Karchev <x_karchev@mail.bg>
2021-11-29 16:59:51 +0200

    Static SignIn page created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './pages';
+import SigninPage from './pages/signin';
 
 function App() {
   return (
-    <Home />
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/signin" component={SigninPage} exact />
+    </Switch>
   );
 }
 
