7783a4e8f8b3b81989d212be8a966b2d68b6a8aa
Hristo Karchev <x_karchev@mail.bg>
2021-11-29 10:40:22 +0200

    third info section created and added
    still not finished

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -18,18 +18,35 @@ export const homeObjOne = {
 };
 
 export const homeObjTwo = {
-    id: 'about',
+    id: 'discover',
     lightBg: true,
-    lightText: true,
-    lightTextDesc: true,
-    topLine: 'Premium Bank',
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Unlimited Access',
     headline: 'Unlimited Trans with zero fees',
     description: 'Get access ..... text.',
-    buttonLabel: 'Get started',
-    imgStart: false,
+    buttonLabel: 'Learn More',
+    imgStart: true,
     img: svgOne,
     alt: 'Car',
-    dark: true,
-    primary: true,
-    darkText: false
+    dark: false,
+    primary: false,
+    darkText: true
+};
+
+export const homeObjThree = {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Unlimited Access',
+    headline: 'Unlimited Trans with zero fees',
+    description: 'Get access ..... text.',
+    buttonLabel: 'Learn More',
+    imgStart: true,
+    img: svgOne,
+    alt: 'Car',
+    dark: false,
+    primary: false,
+    darkText: true
 };
