d9103c4fe77eae8ed8765a5aa9ae4d7e352905bf
Hristo Karchev <x_karchev@mail.bg>
2021-11-29 18:48:56 +0200

    Social Media part created for the footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
-
+import {
+    FooterContainer,
+    FooterWrap,
+    FooterLinksContainer,
+    FooterLinksWrapper,
+    FooterLinkItems,
+    FooterLinkTitle,
+    FooterLink
+} from './FooterElements';
 const Footer = () => {
     return (
         <>
@@ -17,11 +25,11 @@ const Footer = () => {
                             </FooterLinkItems>
 
                             <FooterLinkItems>
-                                <FooterLinkTitle>About us</FooterLinkTitle>
-                                <FooterLink to="/signin">How it works</FooterLink>
-                                <FooterLink to="/signin">Test</FooterLink>
-                                <FooterLink to="/signin">Yoga</FooterLink>
-                                <FooterLink to="/signin">Terms of Service</FooterLink>
+                                <FooterLinkTitle>Social Media</FooterLinkTitle>
+                                <FooterLink to="/signin">Instagram</FooterLink>
+                                <FooterLink to="/signin">Facebook</FooterLink>
+                                <FooterLink to="/signin">Twitter</FooterLink>
+                                <FooterLink to="/signin">GitHub</FooterLink>
                             </FooterLinkItems>
 
                         </FooterLinksWrapper>
@@ -37,15 +45,59 @@ const Footer = () => {
                             </FooterLinkItems>
 
                             <FooterLinkItems>
-                                <FooterLinkTitle>About us</FooterLinkTitle>
-                                <FooterLink to="/signin">How it works</FooterLink>
-                                <FooterLink to="/signin">Test</FooterLink>
-                                <FooterLink to="/signin">Yoga</FooterLink>
-                                <FooterLink to="/signin">Terms of Service</FooterLink>
+                                <FooterLinkTitle>Social Media</FooterLinkTitle>
+                                <FooterLink to="/signin">Instagram</FooterLink>
+                                <FooterLink to="/signin">Facebook</FooterLink>
+                                <FooterLink to="/signin">Twitter</FooterLink>
+                                <FooterLink to="/signin">GitHub</FooterLink>
                             </FooterLinkItems>
 
                         </FooterLinksWrapper>
                     </FooterLinksContainer>
+
+                    <SocialMedia>
+                        <SocialMediaWrap>
+                            <SocialLogo to='/'>alpha</SocialLogo>
+                            <WebsiteRights>alpha &copy; {new Date().getFullYear()}
+                                All rights reserved.</WebsiteRights>
+                            <SocialIcons>
+                                <SocialIconLink
+                                    href="/"
+                                    target="_blank"
+                                    aria-label="Facebook">
+                                    <FaFacebook />
+                                </SocialIconLink>
+
+                                <SocialIconLink
+                                    href="/"
+                                    target="_blank"
+                                    aria-label="Instagram">
+                                    <FaInstagram />
+                                </SocialIconLink>
+
+                                <SocialIconLink
+                                    href="/"
+                                    target="_blank"
+                                    aria-label="YouTube">
+                                    <FaYoutube />
+                                </SocialIconLink>
+
+                                <SocialIconLink
+                                    href="/"
+                                    target="_blank"
+                                    aria-label="Twitter">
+                                    <FaTwitter />
+                                </SocialIconLink>
+
+                                <SocialIconLink
+                                    href="/"
+                                    target="_blank"
+                                    aria-label="GitHub">
+                                    <FaGithub />
+                                </SocialIconLink>
+                            </SocialIcons>
+                        </SocialMediaWrap>
+                    </SocialMedia>
                 </FooterWrap>
             </FooterContainer>
         </>
