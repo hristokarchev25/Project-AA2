4e6e99f4c3ad306e79816e391ed058c61d06d7fd
Hristo Karchev <x_karchev@mail.bg>
2023-02-08 15:17:23 +0200

    v.2.0 add/ sending email from contact us form with emailjs

--- src/components/ContactUs/index.js
+++ src/components/ContactUs/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 import {
     Container,
@@ -18,7 +19,15 @@ import {
 import white from '../../images/white.png';
 
 function ContactUs() {
+    const form = useRef();
 
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_7y6ptef', 'template_jw07z6h', form.current, 'F6DpDCZFuE7vcLJRP');
+
+        e.target.reset();
+    };
 
     return (
         <>
@@ -28,15 +37,15 @@ function ContactUs() {
                         <LotusImg src={white} />
                     </Icon>
                     <FormContent>
-                        <Form action="#">
+                        <Form ref={form} onSubmit={sendEmail}>
                             <FormH1>Send us a message!</FormH1>
                             <FormLabel htmlFor='name'>Full name</FormLabel>
-                            <FormInput type='text' placeholder="Type full name here"  required />
+                            <FormInput type='text' name='name' placeholder="Type full name here" required />
                             <FormLabel htmlFor='email'>Email</FormLabel>
-                            <FormInput type='email' placeholder="Type email here" required />
+                            <FormInput type='email' name='email' placeholder="Type email here" required />
                             <FormLabel htmlFor='message'>Message</FormLabel>
-                            <FormTextArea placeholder="Type your message here" required />
-                            <FormButton type='button'>SUBMIT</FormButton>
+                            <FormTextArea name='message' placeholder="Type your message here" required />
+                            <FormButton type='submit'>SUBMIT</FormButton>
                             <Text>Send us a message and we'll respond within 24 hours.</Text>
                         </Form>
                     </FormContent>
